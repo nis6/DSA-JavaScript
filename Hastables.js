872cc461e4ffd487e1f5fe5992b926faa258fb18
nis6 <nishachauhan0950@gmail.com>
2021-11-23 23:14:21 +0530

    hashtable complete-DS1

--- Hastables.js
+++ Hastables.js
@@ -1,43 +1,90 @@
 class Hashtable{
     constructor(){
         this.table={};
-        this.size=20;
-        //this.slots=0;
+        this.maxsize=127;
     }
-
-    hash_function(key){
-        return key%this.size;
+    //when key is a number
+    _hash_arr(key){
+        return key%this.maxsize;
     }
-    
-    insert(key,val){
-        let hash=this.hash_function(key);
-        while(this.table[hash]!=undefined){
-            hash=this.hash_function(hash+1); 
+    //when key is a string
+    _has_associative_arr(key){
+        let hash=0;
+        for(let i=0;i<key.length;i++){
+            hash+=key[i];
         }
-        this.table[hash]=val;
+        return hash>this.maxsize; //to keep the hash value less than bucket size
+    }
+    //final hash function
+    _hash(key){
+        if(typeof key === 'number')
+        return this._hash_arr(key);
+        else if(typeof key === 'string')
+        return this._has_associative_arr(key);
+    }
+    //value itself is key, if you want to use seperate key value then use direct 
+    insert_array(arr){
+        for(let i=0;i<nums1.length;i++){
+            this.insert(arr[i],arr[i]);
+        }
+    }
+    insert_obj(obj){
+        this.table=obj;
     }
 
+    //insert into hash table
+    insert(key,val){
+        let index=this._hash(key);
+        while(this.table[index]!=undefined && index>this.maxsize)
+            index=this._hash(index+1); 
+        this.table[index]=val;
+    }
+    //search a key for value and value for key
     search(key,val){
-        let hash=this.hash_function(key);
-        while(this.table[hash]!=val && this.table[hash]!="")
-        {hash=this.hash_function(hash+1)}
-        if(this.table[hash]==val)
-            return val;
-        else
-            return false;
-   }
+        if(arguments[0]){
+            let index=this._hash(key);
+            while(this.table[index]!=undefined && index>this.maxsize){
+                if(this.table[index]) return this.table[index];
+                index=this._hash(index+1); 
+            }
+        }
+        else{
+            let values=Object.values(this.table);
+            if(values.includes(val)) return Object.keys(this.table)[values.indexOf(val)];
+        }        
+    }
+    //delete for any given key or value pass undefined for unknown
+    dlt(key,val){
+        if(arguments[0]){
+            let index=this._hash(key);
+            while(this.table[index]!=undefined && index>this.maxsize){
+                if(this.table[index]==val){
+                    delete this.table[index];
+                    return "deleted"
+                }
+                else index=this._hash(index+1); 
+            }
+            return "key doesn't exist"
+        }
+        else{
+            let values=Object.values(this.table);       //
+            if(values.includes(val)){                  //redundant code, for function ref tho
+                for(let i in this.table){
+                    if(this.table[i]==val)
+                    {delete this.table[i];
+                    return "deleted"; }
+                }
+            }
+            return "value doesn't exist";
+        }    
+    }
 }
 
-nums1=[9,4,9,8,4];
-nums2=[4,9,5];
+nums1=[932,474,921,812,114];
+
 let ht=new Hashtable();
-for(let i=0;i<nums1.length;i++)
-{
-    ht.insert(nums1[i],nums1[i]);
-}
-let result=[];
-for(let i=0;i<nums2.length;i++){
-    if(ht.hasOwnProperty(ht.hash_function(nums2[i])))
-        result.push(nums2[i]);
-}
-console.log(result);
+
+ht.insert_array(nums1);
+
+console.log(ht.search(undefined,932));
+console.log(ht.table);
