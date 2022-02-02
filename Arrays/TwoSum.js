d7357716e1d8aa393e71b4100be0a0b5f33309eb
Nisha Chauhan <nishachauhan0950@gmail.com>
2022-02-02 22:23:39 +0530

    Runtime: 64 ms, faster than 99.48% of JavaScript online submissions for Two Sum.

--- Arrays/TwoSum.js
+++ Arrays/TwoSum.js
@@ -1,4 +1,14 @@
 var twoSum = function(nums, target) {
+  let ht={}
+  for(let i in nums){
+      let diff=target-nums[i];
+      if(ht.hasOwnProperty(nums[i])) return [i, ht[nums[i]]];
+      else ht[diff]=i;
+  }
+};
+
+//Alternate Solution, -returns number rather than index -multiple pairs rather than a single one 
+var twoSum = function(nums, target) {
     const hash={};
     const twoSumArr=[];
     for(let i in nums){
@@ -9,8 +19,9 @@ var twoSum = function(nums, target) {
       else{
         hash[diff]=nums[i];
       }
+      console.log(hash);
     }
+    
      return twoSumArr;
   };
-  
 console.log(twoSum([2,4,7,8,9],11));
